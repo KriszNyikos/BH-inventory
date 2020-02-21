3bad83a3de28f270ea3526c1eefbae123ce65645
Krisztian Nyikos <krisz.nyikos@gmail.com>
2020-02-21 23:29:09 +0100

    2020 02 21 database commit

--- dbinit.js
+++ dbinit.js
@@ -8,20 +8,55 @@ const db = new sqlite3.Database('./inventory', (err) => {
 });
 
 const products = [
-    { id: 1, name: "Processzor", grouppi: 'számitástechnika' },
-    { id: 2, name: "Sütő", grouppi: 'konyhatechnika' },
-    { id: 3, name: "Kazán", grouppi: 'fűtestechnika' },
-    { id: 4, name: "Függöny", grouppi: 'árnyékolástechnika' },
+    { id: 1, name: "Processzor", grouppi: 'pc' },
+    { id: 2, name: "Sütő", grouppi: 'kitchen' },
+    { id: 3, name: "Kazán", grouppi: 'warm' },
+    { id: 4, name: "Függöny", grouppi: 'shadow' },
 ]
 
+const inventory = [
+    {product_id: 1, quantity: 30},
+    {product_id: 2, quantity: 71},
+    {product_id: 3, quantity: 863},
+    {product_id: 4, quantity: 73}
+]
+
+
 db.serialize(function () {
-    db.run("CREATE TABLE IF NOT EXISTS products ('id' VARCHAR(100),'name' VARCHAR(100), 'grouppi' VARCHAR(100))");
+    db.run("DROP TABLE products");
+    db.run("DROP TABLE inventory");
 })
+
+db.serialize(function () {
+    db.run("CREATE TABLE IF NOT EXISTS products ('id' INTEGER PRIMARY KEY NOT NULL, 'name' VARCHAR(100), 'grouppi' VARCHAR(100));");
+})
+
+db.serialize(function () {
+    db.run("CREATE TABLE IF NOT EXISTS inventory ('quantity' INTEGER, 'product_id' INTEGER NOT NULL, FOREIGN KEY (product_id) REFERENCES products(id));");
+})
+
 
 db.serialize(function(){
     products.forEach( e => {
-        console.log(e)
-        db.prepare('INSERT INTO products VALUES (?, ?, ?)')
+        db.prepare(`INSERT INTO products VALUES (?,?,?)`)
         .run(e.id, e.name, e.grouppi)
+    })
+})
+
+db.serialize(function(){
+    inventory.forEach( e => {
+        db.prepare('INSERT INTO inventory VALUES (?,?)')
+        .run(e.quantity, e.product_id)
+    })
+})
+
+db.serialize(function () {
+
+    db.all("SELECT p.id, p.name, i.quantity FROM products p INNER JOIN inventory i ON i.product_id = p.id ;", function (err, results) {
+        if (err != null) {
+            // hibakezelés
+        }
+        console.log(results)
+    
     })
 })
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 4000
+const port = 3000
 const sqlite3 = require('sqlite3').verbose();
+
 
 const db = new sqlite3.Database('./inventory', (err) => {
     if (err) {
@@ -12,50 +13,74 @@ const db = new sqlite3.Database('./inventory', (err) => {
 
 const exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({
-    defaultLayout: 'main', 
+    defaultLayout: 'main',
     layoutsDir: __dirname + '/views/layouts/',
     partialsDir: __dirname + '/views/partials/'
 }
-    
+
 ));
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 app.use(express.static('views/images'));
-
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/products', (req, res) => {
 
-   db.serialize(function(){
-        
-        db.all("SELECT * FROM products", function(err, results) {
+    db.serialize(function () {
+
+        db.all("SELECT * FROM products", function (err, results) {
             if (err != null) {
                 // hibakezelés
             }
             console.log(results)
-            res.render('products', {array: results})
+            res.render('nav', {
+                array: results,
+                products: true
+            })
         })
     })
 })
 
-app.post('/products', (req, res)=>{
+app.post('/products', (req, res) => {
+    console.log(req.body)
+    var  last_id = 0
+
+    db.serialize(function () {
+        db.all('SELECT COUNT(id) FROM products', (err, result) =>{
+            last_id = result[0]['COUNT(id)'] + 1
+
+            db.prepare('INSERT INTO products VALUES (?, ?, ?)')
+            .run(last_id, req.body.name, req.body.catlist)
+
+            db.prepare('INSERT INTO inventory VALUES (?, ?)')
+            .run(0, last_id)
+
+            console.log(last_id)
+        })
+        
+    })
 
 })
 
 app.get('/stocks', (req, res) => {
 
-   db.serialize(function(){
-        
-        db.all("SELECT * FROM products", function(err, results) {
+    db.serialize(function () {
+
+        db.all("SELECT p.id, p.name, i.quantity FROM products p INNER JOIN inventory i ON i.product_id = p.id ;", function (err, results) {
             if (err != null) {
                 // hibakezelés
             }
             console.log(results)
-            res.render('stocks', {array: results})
+            res.render('nav', {
+                array: results,
+                stocks: true
+            })
         })
     })
 
-    
-    
+
+
 })
 
 

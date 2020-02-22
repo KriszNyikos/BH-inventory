b6229eeb0f50ae020c6846a70e8580867a98ac80
Krisztian Nyikos <krisz.nyikos@gmail.com>
2020-02-22 17:55:16 +0100

    2020 02 22 group_in_progress

--- dbinit.js
+++ dbinit.js
@@ -8,10 +8,10 @@ const db = new sqlite3.Database('./inventory', (err) => {
 });
 
 const products = [
-    { id: 1, name: "Processzor", grouppi: 'pc' },
-    { id: 2, name: "Sütő", grouppi: 'kitchen' },
-    { id: 3, name: "Kazán", grouppi: 'warm' },
-    { id: 4, name: "Függöny", grouppi: 'shadow' },
+    { id: 1, name: "Processzor", cat_id: 1, desc: 'itt a sok habi' },
+    { id: 2, name: "Sütő", cat_id: 2, desc: 'habihabi' },
+    { id: 3, name: "Kazán", cat_id: 3, desc: 'ideis' },
+    { id: 4, name: "Függöny", cat_id: 4, desc: 'nanemááááááá' },
 ]
 
 const inventory = [
@@ -21,14 +21,37 @@ const inventory = [
     {product_id: 4, quantity: 73}
 ]
 
+const categories = [
+    { id: 1, catname: 'Számítástechnika' },
+    { id: 2, catname: 'Konyhatechnika' },
+    { id: 3, catname: 'Fűtéstechnika'},
+    { id: 4, catname: 'Árnyékolástechnika'},
+]
+
+const pro_cat = [
+    {pro: 1, cat : 1},
+    {pro: 2, cat : 2},
+    {pro: 3, cat : 3},
+    {pro: 4, cat : 4}
+
+]
 
 db.serialize(function () {
     db.run("DROP TABLE products");
     db.run("DROP TABLE inventory");
+    db.run("DROP TABLE categories");
 })
 
 db.serialize(function () {
-    db.run("CREATE TABLE IF NOT EXISTS products ('id' INTEGER PRIMARY KEY NOT NULL, 'name' VARCHAR(100), 'grouppi' VARCHAR(100));");
+    db.run("CREATE TABLE IF NOT EXISTS categories ('name' TEXT, 'id' INTEGER PRIMARY KEY NOT NULL);");
+})
+
+db.serialize(function () {
+    db.run("CREATE TABLE IF NOT EXISTS pro_cat ('product_id' INTEGER, 'cat_id' INTEGER);");
+})
+
+db.serialize(function () {
+    db.run("CREATE TABLE IF NOT EXISTS products ('id' INTEGER PRIMARY KEY NOT NULL, 'name' VARCHAR(100), 'desc' VARCHAR(200));");
 })
 
 db.serialize(function () {
@@ -36,10 +59,26 @@ db.serialize(function () {
 })
 
 
+
+db.serialize(function(){
+    pro_cat.forEach( e => {
+        db.prepare(`INSERT INTO pro_cat VALUES (?,?)`)
+        .run(e.pro, e.cat)
+    })
+})
+
+db.serialize(function(){
+    categories.forEach( e => {
+        db.prepare(`INSERT INTO categories VALUES (?,?)`)
+        .run(e.catname, e.id)
+    })
+})
+
+
 db.serialize(function(){
     products.forEach( e => {
         db.prepare(`INSERT INTO products VALUES (?,?,?)`)
-        .run(e.id, e.name, e.grouppi)
+        .run(e.id, e.name, e.desc )
     })
 })
 
@@ -52,11 +91,24 @@ db.serialize(function(){
 
 db.serialize(function () {
 
-    db.all("SELECT p.id, p.name, i.quantity FROM products p INNER JOIN inventory i ON i.product_id = p.id ;", function (err, results) {
+    db.all("SELECT p.id, p.name AS productname, i.quantity, p.desc, c.id AS cat_id, c.name AS cat_name FROM categories c INNER JOIN products p ON p.cat_id = c.id INNER JOIN inventory i ON i.product_id = p.id;", function (err, result) {
         if (err != null) {
             // hibakezelés
         }
-        console.log(results)
+        console.log(result)
     
     })
 })
+/*
+db.serialize(function () {
+
+    db.all("SELECT p.id, p.name AS productname, i.quantity, p.desc, c.id AS cat_id, c.name AS cat_name FROM products p INNER JOIN inventory i ON i.product_id = p.id INNER JOIN pro_cat ON pro_cat product_id =;", function (err, result) {
+        if (err != null) {
+            // hibakezelés
+        }
+        console.log(result)
+    
+    })
+})
+
+*/
--- index.js
+++ index.js
@@ -28,37 +28,89 @@ app.use(express.urlencoded({ extended: true }))
 app.get('/products', (req, res) => {
 
     db.serialize(function () {
-
-        db.all("SELECT * FROM products", function (err, results) {
+        db.all("SELECT c.name AS catName, p.id, p.name, p.desc FROM categories c INNER JOIN products p ON p.cat_id = c.id", function (err, results) {
             if (err != null) {
                 // hibakezelés
             }
-            console.log(results)
-            res.render('nav', {
-                array: results,
-                products: true
+
+            db.all('SELECT * FROM categories', function (err, cat) {
+
+                console.log(results)
+                console.log(cat)
+
+                if (err != null) {
+                    // hibakezelés
+                }
+                res.render('nav', {
+                    cat: cat,
+                    array: results,
+                    products: true
+                })
             })
         })
     })
 })
 
+app.delete('/products', (req, res) => {
+    console.log(req.body)
+    db.serialize(function () {
+
+        db.run(`DELETE FROM inventory WHERE product_id = ${parseInt(req.body.id)}`, function (err) {
+            if (err) {
+                return console.error(err.message);
+            }
+        });
+
+        db.run(`DELETE FROM products WHERE id = ${parseInt(req.body.id)}`, function (err) {
+            if (err) {
+                return console.error(err.message);
+            }
+            console.log(`Row(s) deleted: ${this.changes}`);
+            res.sendStatus(200)
+        });
+
+    })
+})
+
+
 app.post('/products', (req, res) => {
     console.log(req.body)
-    var  last_id = 0
+    var last_id = 0
 
     db.serialize(function () {
-        db.all('SELECT COUNT(id) FROM products', (err, result) =>{
+        db.all('SELECT COUNT(id) FROM products', (err, result) => {
             last_id = result[0]['COUNT(id)'] + 1
 
-            db.prepare('INSERT INTO products VALUES (?, ?, ?)')
-            .run(last_id, req.body.name, req.body.catlist)
+            db.prepare('INSERT INTO products VALUES (?, ?, ?, ?)')
+                .run(last_id, req.body.name, req.body.description, parseInt(req.body.catlist))
 
             db.prepare('INSERT INTO inventory VALUES (?, ?)')
-            .run(0, last_id)
+                .run(0, last_id)
 
-            console.log(last_id)
+            res.redirect('/products')
         })
-        
+
+    })
+
+})
+
+app.post('/products/:id', (req, res) => {
+    console.log(req.body)
+    console.log(req.params)
+
+    if (req.body.catlist == 'none') {
+        res.send('Nincs kategória megadva')
+    }
+
+    db.serialize(function () {
+        db.run(`UPDATE products SET name = '${req.body.name}', cat_id = '${parseInt(req.body.catlist)}', desc='${req.body.description}' WHERE id = ${parseInt(req.params.id)}`, function (err) {
+            if (err) {
+                return console.error(err.message);
+            }
+            console.log(`Row(s) updated: ${this.changes}`);
+            res.redirect('/products')
+        });
+
     })
 
 })
@@ -66,6 +118,14 @@ app.post('/products', (req, res) => {
 app.get('/stocks', (req, res) => {
 
     db.serialize(function () {
+
+        db.all("SELECT * FROM inventory;", function (err, results) {
+            if (err != null) {
+                // hibakezelés
+            }
+            console.log(results)
+
+        })
 
         db.all("SELECT p.id, p.name, i.quantity FROM products p INNER JOIN inventory i ON i.product_id = p.id ;", function (err, results) {
             if (err != null) {
@@ -78,9 +138,112 @@ app.get('/stocks', (req, res) => {
             })
         })
     })
+})
 
 
+app.post('/stocks/:id', (req, res) => {
+    db.serialize(function () {
+        db.run(`UPDATE inventory SET quantity = ${req.body.quantity} WHERE product_id = ${req.params.id}`, function (err) {
+            if (err) {
+                return console.error(err.message);
+            }
+            console.log(`Row(s) updated: ${this.changes}`);
+            res.redirect('/stocks')
+        });
+    })
+})
 
+
+app.get('/categories', (req, res) => {
+
+    db.serialize(function () {
+
+        db.all('SELECT * FROM categories', function (err, results) {
+
+            console.log(results)
+
+            if (err != null) {
+                // hibakezelés
+            }
+            res.render('nav', {
+                array: results,
+                categories: true
+            })
+        })
+
+    })
+})
+
+
+app.post('/categories', (req, res) => {
+    console.log(req.body)
+    var last_id = 0
+
+    db.serialize(function () {
+        db.all('SELECT COUNT(id) FROM products', (err, result) => {
+            last_id = result[0]['COUNT(id)'] + 1
+
+            db.prepare('INSERT INTO categories VALUES (?, ?)')
+                .run(req.body.name, last_id)
+
+            res.redirect('/categories')
+        })
+
+    })
+
+})
+
+app.post('/categories/:id', (req, res) => {
+    db.serialize(function () {
+        db.run(`UPDATE categories SET name = '${req.body.name}' WHERE id = ${parseInt(req.params.id)}`, function (err) {
+            if (err) {
+                return console.error(err.message);
+            }
+            console.log(`Row(s) updated: ${this.changes}`);
+            res.redirect('/categories')
+        });
+    })
+})
+
+app.delete('/categories', (req, res) => {
+    console.log(req.body)
+    db.serialize(function () {
+
+        db.all(`SELECT p.id, c.id FROM products p INNER JOIN categories c ON c.id = p.cat_id WHERE c.id = ${req.body.id}`, function (err, results) {
+
+            if (err != null) {
+                // hibakezelés
+            }
+            console.log(results[0])
+
+            delId = results[0].id
+
+           db.run(`DELETE FROM inventory WHERE product_id = ${parseInt(delId)}`, function (err) {
+                if (err) {
+                    return console.error(err.message);
+                }
+                console.log(`Row(s) deleted: ${this.changes}`);
+            });
+
+            db.run(`DELETE FROM products WHERE id = ${parseInt(delId)}`, function (err) {
+                if (err) {
+                    return console.error(err.message);
+                }
+                console.log(`Row(s) deleted: ${this.changes}`);
+            });
+
+            db.run(`DELETE FROM categories WHERE id = ${parseInt(req.body.id)}`, function (err) {
+                if (err) {
+                    return console.error(err.message);
+                }
+                console.log(`Row(s) deleted: ${this.changes}`);
+            });
+
+            res.redirect('/categories')
+
+        
+        })
+    })
 })
 
 
